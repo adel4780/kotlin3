2ef3c109aaa4b65130d920fe0d3e347d56a0bd75
adel4780 <adelk138019@gmail.com>
2024-05-23 09:24:19 +0330

    add t3 to StatusCheckWorker

--- app/src/main/java/com/example/android_bc_service_worker_tutorial/StatusCheckWorker.kt
+++ app/src/main/java/com/example/android_bc_service_worker_tutorial/StatusCheckWorker.kt
@@ -10,6 +10,11 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import org.json.JSONObject
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class StatusCheckWorker(appContext: Context, workerParams: WorkerParameters) :
@@ -39,9 +44,26 @@ class StatusCheckWorker(appContext: Context, workerParams: WorkerParameters) :
         Log.i("worker_bluetooth", "Bluetooth is ${if (isBluetoothEnabled) "Enabled" else "Disabled"}")
         Log.i("worker_airplane", "Airplane mode is ${if (isAirplaneModeOn) "On" else "Off"}")
         rescheduleWork()
+        writeLogToFile(isBluetoothEnabled, isAirplaneModeOn)
         return Result.success()
     }
+
+    private fun writeLogToFile(isBluetoothEnabled: Boolean, isAirplaneModeOn: Boolean) {
+        val logFile = File(applicationContext.filesDir, "status_logs.txt")
+        val jsonObject = JSONObject()
+        jsonObject.put("timestamp", convertMillisToDate(System.currentTimeMillis()))
+        jsonObject.put("bluetooth_enabled", isBluetoothEnabled)
+        jsonObject.put("airplane_mode_on", isAirplaneModeOn)
+        val jsonString = jsonObject.toString() + "\n"
+        logFile.appendText(jsonString)
+    }
+
+    fun convertMillisToDate(timestamp: Long): String {
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return sdf.format(Date(timestamp))
+    }
 }
+
 
 fun scheduleInitialWork(context: Context) {
     val constraints = Constraints.Builder()
